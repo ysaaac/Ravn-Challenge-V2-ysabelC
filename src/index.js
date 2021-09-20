0e59307d0e10e54374f1f144ad726791b1558ba7
ysaaac <cyysabel@gmail.com>
2021-09-19 23:00:46 -0500

    add-apollo-client

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+    cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App/>
+        </ApolloProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
